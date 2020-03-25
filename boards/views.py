fdbf79512ebbf4416ad30adbf1e1de4507f559d7
afrunk <afrunk7@gmail.com>
2020-03-25 14:45:05 +0800

    form self

--- boards/views.py
+++ boards/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 # 导入 HttpResponse 对象 由 DJango 自动创建
 from django.http import HttpResponse,JsonResponse
 # 导入时间模块
 import datetime
 # 导入 Board 为数据调用做准备
-from .models import Board
+from .models import Board,Topic,Post
 import json
+
+from django.contrib.auth.models import  User
 
 # Create your views here.
 # 首页的视图函数
@@ -34,4 +36,24 @@ def board_topics_2(request,pk):
 # 新建 topic 的函数
 def new_topic(request,pk):
     board = get_object_or_404(Board,pk=pk)
+
+    if request.method =='POST':
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        user = User.objects.first() # 临时使用一个账号作为登录用户
+
+        topic = Topic.objects.create(
+            subject = subject,
+            board = board,
+            starter= user
+        )
+
+        post = Post.objects.create(
+            message = message,
+            topic = topic,
+            created_by=user
+        )
+        # 重定向
+        return redirect('board_topics2',pk=board.pk)
     return render(request,'new_topic.html',{'board':board})
